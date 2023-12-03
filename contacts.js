26869764dab80ea883b4b1ff443c177cc7a3b64e
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-12-03 23:09:57 +0100

    added without comments

--- contacts.js
+++ contacts.js
@@ -1,20 +1,13 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-// const contactsPath = path.resolve("db/contacts.json");
-// const contactsPath = path.join("db", "contacts.json");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 console.log(contactsPath);
 
-// TODO: задокументувати кожну функцію
 async function listContacts() {
   try {
-    // ...твій код. Повертає масив контактів.
     const readResult = await fs.readFile(contactsPath);
-    // console.log(readResult);
     const arrayContacts = JSON.parse(readResult);
-    // const arrayContacts = readResult.toString();
-    // console.log("listContacts:", arrayContacts);
     return arrayContacts;
   } catch (err) {
     console.log(err);
@@ -24,24 +17,20 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   try {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
 
     const foundContactById = contacts.find(
       (contact) => contact.id === contactId
     );
-    // console.log("foundContactById:", foundContactById || null);
     return foundContactById || null;
   } catch (err) {
     console.error(err);
     throw err;
   }
 }
-// getContactById("rsKkOQUi80UsgVPCcLZZW");
 
 async function removeContact(contactId) {
   try {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
 
     const findContactIndex = contacts.findIndex(
@@ -53,7 +42,6 @@ async function removeContact(contactId) {
       return null;
     } else {
       const removeContactById = contacts.splice(findContactIndex, 1)[0];
-      // console.log("removeContact:", removeContactById);
       return removeContactById;
     }
   } catch (err) {
@@ -61,11 +49,9 @@ async function removeContact(contactId) {
     throw err;
   }
 }
-// removeContact("rsKkOQUi80UsgVPCcLZZW");
 
 async function addContact(name, email, phone) {
   try {
-    // ...твій код. Повертає об'єкт доданого контакту.
     const contacts = await listContacts();
 
     const newContactId = Date.now().toString();
@@ -86,7 +72,6 @@ async function addContact(name, email, phone) {
     throw err;
   }
 }
-// addContact("Pavlo", "abec", "555");
 
 module.exports = {
   listContacts,
@@ -94,20 +79,3 @@ module.exports = {
   removeContact,
   addContact,
 };
-
-// console.log(global);
-// console.log(__dirname);
-// console.log(__filename);
-
-// різниця контекст, в стрілочній не має this 1:03
-// const contactsPath = () => console.log("HI");
-// functions експрешин, обявити можна тільки піся бо тут є const
-// const contactsPath = function () {
-//   console.log("HI");
-// };
-// functions declarations, можемо викликати до того як обявили
-// function contactsPath(params) {}
-
-// (async () => {
-//   console.log("TEST");
-// })();
